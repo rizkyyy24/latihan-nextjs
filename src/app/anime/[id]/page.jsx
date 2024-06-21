a9d4f78baaf05958afee695bdf9582fe8d6599b8
Rizky Yoga <rizkyyoga11@gmail.com>
2024-06-21 16:31:06 +0700

    second commit

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -11,6 +11,20 @@ const AnimeDetails = async ({ params }) => {
       <div className="text-color-primary text-2xl text-center">
         {animeDetails.data.title} - {animeDetails.data.year}
       </div>
+      <div className="p-4 flex gap-2 text-color-primary items-center justify-center">
+        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary px-3 py-2">
+          <h3>PERINGKAT</h3>
+          <p>{animeDetails.data.rank}</p>
+        </div>
+        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary px-3 py-2">
+          <h3>SKOR</h3>
+          <p>{animeDetails.data.score}</p>
+        </div>
+        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary px-3 py-2">
+          <h3>EPISODE</h3>
+          <p>{animeDetails.data.episodes}</p>
+        </div>
+      </div>
       <div className="flex flex-wrap text-color-primary">
         <div className="sm:w-1/3 w-full flex justify-center sm:items-center p-4">
           <Image
